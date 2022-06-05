d6efae351b88fdfd83efcd8a775d32891be7ec3a
brs <brv1114@yahoo.com>
2022-06-04 20:52:49 -0500

    repaired CSS bug

---
+++ assets/scheduler.js
@@ -0,0 +1,22 @@
+var textContent;
+
+//display date and time up top
+var displayTime = document.getElementById("timeofday")
+var currentTime = moment();
+
+displayTime.textContent = currentTime.format("MMMM DD YYYY");
+
+
+
+
+
+
+//Add local storage
+
+//Add to keep storage on page refresh
+
+//Add color function when time is past selector
+
+//Add color function when time is current to selector
+
+//Add on click function for save 
